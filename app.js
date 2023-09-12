10e9c72708e6667a91acd022a3e1777392f08724
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2023-09-12 00:04:33 -0300

    render

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-app.get('/', (req, res) => {   res.send('Jobs API') })
+// app.get('/', (req, res) => {   res.send('Jobs API') })
 
 const port = process.env.PORT || 3000;
 
