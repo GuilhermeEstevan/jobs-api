0fa86bad3afe49f05d514fbf5873237308fb5430
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2023-09-12 09:55:44 -0300

    app.js

--- app.js
+++ app.js
@@ -17,22 +17,14 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 
 // extra security packages
 import helmet from 'helmet';
-import cors from 'cors'
 import { xss } from 'express-xss-sanitizer';
-import rateLimiter from 'express-rate-limit';
 
-app.set('trust proxy', 1)
-const limiter = rateLimiter({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 
-})
 
 app.use(express.json());
 app.use(helmet())
-app.use(cors())
 app.use(xss())
-app.use(limiter)
+
 
 // routes
 app.use('/api/v1/auth', authRouter)
